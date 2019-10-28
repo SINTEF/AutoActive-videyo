35754de61cb143f26d14812c3eb82335ff148d13
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2019-10-28 16:10:10 +0100

    Fixed release version

--- VideYo/MainWindow.xaml.cs
+++ VideYo/MainWindow.xaml.cs
@@ -4,9 +4,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
 using Microsoft.Win32;
 using System.IO;
 using System.Text;
@@ -22,7 +19,6 @@ namespace VideYo
     public partial class MainWindow : Window
     {
         private readonly IList<FileItem> _items = new ObservableCollection<FileItem>();
-        private readonly Task _ffmpegGetterTask;
         private TimeSpan _totalLength;
 
         public MainWindow()
@@ -30,10 +26,7 @@ namespace VideYo
             InitializeComponent();
 
             listBox.DataContext = _items;
-
-            _ffmpegGetterTask = FFmpeg.GetLatestVersion();
         }
-
 
         private async Task EnqueueFile(string fileName)
         {
@@ -67,9 +60,8 @@ namespace VideYo
 
         private async void Add_Clicked(object sender, RoutedEventArgs e)
         {
+            await EnsureFfmpeg();
             var fileDialog = new OpenFileDialog {Multiselect = true};
-
-            await _ffmpegGetterTask;
 
             var result = fileDialog.ShowDialog();
             if (!(result.HasValue && result.Value))
@@ -81,14 +73,17 @@ namespace VideYo
             {
                 await EnqueueFile(fileName);
             }
-
         }
-
-
 
         private async void Process_Clicked(object sender, RoutedEventArgs e)
         {
-            await _ffmpegGetterTask;
+            if (!_items.Any())
+            {
+                MessageBox.Show("No videos selected.");
+                return;
+            }
+
+            await EnsureFfmpeg();
 
             int? height = null;
             if (int.TryParse(HeightBox.Text, out var heightText))
@@ -157,110 +152,20 @@ namespace VideYo
             Application.Current.Dispatcher?.Invoke(() =>
                 ProgressBar.Value = 100 * args.Duration.TotalSeconds / _totalLength.TotalSeconds);
         }
+
+        private async Task EnsureFfmpeg()
+        {
+            if (File.Exists("ffmpeg.exe"))
+            {
+                return;
+            }
+
+            MessageBox.Show("ffmpeg.exe not found, downloading. This might take some time.");
+            await FFmpeg.GetLatestVersion(true);
+        }
     }
 
     public class FileItemDragAndDropListBox : DragAndDropListBox<FileItem> { }
-
-    public class DragAndDropListBox<T> : ListBox
-       where T : class
-    {
-        private Point _dragStartPoint;
-        private bool _isDragging;
-
-        private static TP FindVisualParent<TP>(DependencyObject child) where TP : DependencyObject
-        {
-            while (true)
-            {
-                var parentObject = VisualTreeHelper.GetParent(child);
-                switch (parentObject)
-                {
-                    case null:
-                        return null;
-                    case TP parent:
-                        return parent;
-                    default:
-                        child = parentObject;
-                        break;
-                }
-            }
-        }
-
-        public DragAndDropListBox()
-        {
-            PreviewMouseMove += ListBox_PreviewMouseMove;
-
-            var style = new Style(typeof(ListBoxItem));
-
-            style.Setters.Add(new Setter(AllowDropProperty, true));
-
-            style.Setters.Add(new EventSetter(PreviewMouseLeftButtonDownEvent,
-                new MouseButtonEventHandler(ListBoxItem_PreviewMouseLeftButtonDown)));
-
-            style.Setters.Add(new EventSetter(DropEvent, new DragEventHandler(ListBoxItem_Drop)));
-
-            ItemContainerStyle = style;
-        }
-
-        private void ListBox_PreviewMouseMove(object sender, MouseEventArgs e)
-        {
-            if (!_isDragging) return;
-
-            var point = e.GetPosition(null);
-            var diff = _dragStartPoint - point;
-            if (e.LeftButton != MouseButtonState.Pressed ||
-                (!(Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance) &&
-                 !(Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance))) return;
-
-            var lbi = FindVisualParent<ListBoxItem>((DependencyObject)e.OriginalSource);
-            if (lbi != null)
-            {
-                DragDrop.DoDragDrop(lbi, lbi.DataContext, DragDropEffects.Move);
-            }
-        }
-
-        private void ListBoxItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _isDragging = true;
-            _dragStartPoint = e.GetPosition(null);
-        }
-
-        private void ListBoxItem_Drop(object sender, DragEventArgs e)
-        {
-            _isDragging = false;
-            if (!(sender is ListBoxItem listBoxItem)) return;
-
-            var source = e.Data.GetData(typeof(T)) as T;
-            var target = listBoxItem.DataContext as T;
-            if (source == null || target == null)
-                return;
-
-            var sourceIndex = Items.IndexOf(source);
-            var targetIndex = Items.IndexOf(target);
-
-            Move(source, sourceIndex, targetIndex);
-        }
-
-        private void Move(T source, int sourceIndex, int targetIndex)
-        {
-            if (sourceIndex < targetIndex)
-            {
-                if (!(DataContext is IList<T> items)) return;
-
-                items.Insert(targetIndex + 1, source);
-                items.RemoveAt(sourceIndex);
-            }
-            else
-            {
-                if (!(DataContext is IList<T> items)) return;
-
-                var removeIndex = sourceIndex + 1;
-                if (items.Count + 1 <= removeIndex) return;
-
-                items.Insert(targetIndex, source);
-                items.RemoveAt(removeIndex);
-            }
-        }
-    }
 
     public class FileItem
     {
